5d07fb034fa909add78018dcf7753c2f15fb5832
mrxu007 <3202108909@qq.com>
2019-10-13 09:57:31 +0800

    提示安全性改为https访问

--- shop/config/api.config.js
+++ shop/config/api.config.js
@@ -1,5 +1,5 @@
 const isProdMode = Object.is(process.env.NODE_ENV,'production');
 // 判断是开发环境还是上线环境
 module.exports = {
-  baseUrl: isProdMode ? 'http://jsone.xyz/api/' : 'api/'
+  baseUrl: isProdMode ? 'https://jsone.xyz/api/' : 'api/'
 }
